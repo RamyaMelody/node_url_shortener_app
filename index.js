525f1ee7a95473e1277452f90836226375edb6ec
RamyaMelody <61426847+RamyaMelody@users.noreply.github.com>
2020-04-25 00:26:02 +0530

    comit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyparser = require('body-parser');
 var cors = require('cors');
 const app = express();
 const MongoClient = require('mongodb');
-const url = 'mongodb://localhost:27017';
+const url ='mongodb+srv://ramyabtech19:jaisriram@ecomdb-t8ic5.mongodb.net/test?retryWrites=true&w=majority';
 
 app.use(cors());
 
